d3c53b21a8fa9bfced2ae633b7ddcfbc831b36ae
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-08-23 15:30:27 +0200

    feat: switch to Ed25519 keys which seem to be more popular

--- src/derived-key-generation.service.ts
+++ src/derived-key-generation.service.ts
@@ -1,25 +1,31 @@
-import { HDKey } from '@scure/bip32';
-import { StateProvider } from './state-provider';
-import { Tagged } from 'type-fest';
+import { HDKey } from "micro-key-producer/slip10.js";
+import { StateProvider } from "./state-provider";
+import { Tagged } from "type-fest";
 
 const DerivationPaths = {
-  ServerRequestSigning: "m/0/0",
-  ServerVerifiedRequestSigning: "m/0/1",
+  ServerRequestSigning: "m/0'/0'",
+  ServerVerifiedRequestSigning: "m/0'/1'",
 };
 
 type DerivedKeyId = keyof typeof DerivationPaths;
 
 // Note: Opaque is deprecated in favor of Tagged
-type DerivedPrivateKeys = { [KeyId in DerivedKeyId]: Tagged<Uint8Array, `${KeyId}-private`> };
-type DerivedPublicKeys = { [KeyId in DerivedKeyId]: Tagged<Uint8Array, `${KeyId}-public`> };
+type DerivedPrivateKeys = {
+  [KeyId in DerivedKeyId]: Tagged<Uint8Array, `${KeyId}-private`>;
+};
+type DerivedPublicKeys = {
+  [KeyId in DerivedKeyId]: Tagged<Uint8Array, `${KeyId}-public`>;
+};
 
-export type DerivedPrivateKey<KeyId extends DerivedKeyId> = DerivedPrivateKeys[KeyId];
-export type DerivedPublicKey<KeyId extends DerivedKeyId> = DerivedPrivateKeys[KeyId];
+export type DerivedPrivateKey<KeyId extends DerivedKeyId> =
+  DerivedPrivateKeys[KeyId];
+export type DerivedPublicKey<KeyId extends DerivedKeyId> =
+  DerivedPrivateKeys[KeyId];
 
 export type AnyDerivedPrivateKey = DerivedPrivateKeys[DerivedKeyId];
 export type AnyDerivedPublicKey = DerivedPrivateKeys[DerivedKeyId];
 
-export type DerivedKeyPair<KeyId extends DerivedKeyId> = { 
+export type DerivedKeyPair<KeyId extends DerivedKeyId> = {
   publicKey: DerivedPrivateKey<KeyId>;
   privateKey: DerivedPublicKey<KeyId>;
 };
@@ -27,7 +33,10 @@ export type DerivedKeyPair<KeyId extends DerivedKeyId> = {
 export class DerivedKeyGenerationService {
   constructor(private stateProvider: StateProvider) {}
 
-  getKeyPair<KeyId extends DerivedKeyId>(userId: string, keyId: KeyId): DerivedKeyPair<KeyId> {
+  getKeyPair<KeyId extends DerivedKeyId>(
+    userId: string,
+    keyId: KeyId
+  ): DerivedKeyPair<KeyId> {
     const userKey = this.stateProvider.getUserKey(userId);
 
     if (userKey === undefined) {
@@ -39,7 +48,7 @@ export class DerivedKeyGenerationService {
     const derivedKey = hdKey.derive(derivationPath);
 
     return {
-      publicKey: derivedKey.publicKey as DerivedPublicKey<KeyId>,
+      publicKey: derivedKey.publicKeyRaw as DerivedPublicKey<KeyId>,
       privateKey: derivedKey.privateKey as DerivedPrivateKey<KeyId>,
     };
   }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { CryptoService } from './crypto.service';
-import { KeyGenerationService } from './key-generation.service';
-import { WebCryptoFunctionService } from './web-crypto-function.service';
-import { StateProvider } from './state-provider';
-import { DerivedKeyGenerationService } from './derived-key-generation.service';
+import { CryptoService } from "./crypto.service";
+import { KeyGenerationService } from "./key-generation.service";
+import { WebCryptoFunctionService } from "./web-crypto-function.service";
+import { StateProvider } from "./state-provider";
+import { DerivedKeyGenerationService } from "./derived-key-generation.service";
 
 async function run() {
   const cryptoFunctionService = new WebCryptoFunctionService();
@@ -10,19 +10,28 @@ async function run() {
   const stateProvider = new StateProvider();
   const cryptoService = new CryptoService(stateProvider, keyGenerationService);
 
-  const derivedKeyGenerationService = new DerivedKeyGenerationService(stateProvider);
+  const derivedKeyGenerationService = new DerivedKeyGenerationService(
+    stateProvider
+  );
 
   const userId = "fakeUserId";
   await cryptoService.initAccount(userId);
 
-  const derivedKeyPair = derivedKeyGenerationService.getKeyPair(userId, "ServerRequestSigning");
+  const derivedKeyPair = derivedKeyGenerationService.getKeyPair(
+    userId,
+    "ServerRequestSigning"
+  );
   console.log(derivedKeyPair);
 
-  // NOTE: Not supported because SubtleCrypto does not support P-256k1 (also known as secp256k1)
-  // This is the curve used by BIP32
-  // const subtleCryptoKey = await global.crypto.subtle.importKey("raw", derivedKeyPair.privateKey, { name: 'ECDSA', namedCurve: 'P-256' }, true, ['sign']);
+  const subtleCryptoKey = await global.crypto.subtle.importKey(
+    "raw",
+    derivedKeyPair.privateKey,
+    { name: "Ed25519" },
+    true,
+    ["sign"]
+  );
 
-  // console.log(subtleCryptoKey);
+  console.log(subtleCryptoKey);
 }
 
 run().catch(console.error);
