109b4f19d3d78f13cd8fc94fb2ce9bda5dd3427a
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-08-23 15:53:45 +0200

    feat: workaround for Ed25519 import

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { KeyGenerationService } from "./key-generation.service";
 import { WebCryptoFunctionService } from "./web-crypto-function.service";
 import { StateProvider } from "./state-provider";
 import { DerivedKeyGenerationService } from "./derived-key-generation.service";
+import { Utils } from "./utils";
 
 async function run() {
   const cryptoFunctionService = new WebCryptoFunctionService();
@@ -21,11 +22,25 @@ async function run() {
     userId,
     "ServerRequestSigning"
   );
-  console.log(derivedKeyPair);
+
+  // Node doesn't support importing raw keys, so we need to convert it to PKCS8
+  // const subtleCryptoKey = await global.crypto.subtle.importKey(
+  //   "raw",
+  //   derivedKeyPair.privateKey,
+  //   { name: "Ed25519" },
+  //   true,
+  //   ["sign"]
+  // );
+
+  const privateKeyAsHex = Utils.fromBufferToHex(derivedKeyPair.privateKey);
+  // Hacky way to convert raw key to PKCS8 which is just ANS.1 metadata + raw key
+  // Tip: https://lapo.it/asn1js/ is a great tool for ASN.1 decoding
+  const privateKeyAsPkcs8 =
+    "302e020100300506032b657004220420" + privateKeyAsHex;
 
   const subtleCryptoKey = await global.crypto.subtle.importKey(
-    "raw",
-    derivedKeyPair.privateKey,
+    "pkcs8",
+    Utils.fromHexToArray(privateKeyAsPkcs8),
     { name: "Ed25519" },
     true,
     ["sign"]
